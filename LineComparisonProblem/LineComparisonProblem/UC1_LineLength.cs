cca8e4c9e8d16b76de349a1561c91537aefda581
AchonBen <109748551+AchonBen@users.noreply.github.com>
2022-11-28 20:09:12 +0530

    [Steve] -Calculating the Length between Two Points

--- LineComparisonProblem/LineComparisonProblem/UC1_LineLength.cs
+++ LineComparisonProblem/LineComparisonProblem/UC1_LineLength.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LineComparisonProblem
 {
-    internal class UC1_LineLength
+    internal class UC1LineLength
     {
             public static void Distance()
             {
